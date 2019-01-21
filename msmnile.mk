4d937f19841d6bbb955003c65aa1b72512f62de4
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    cepheus: Build livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1
    Signed-off-by: EcrosoftXiao <xy19990903@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -292,6 +292,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msmnile
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
