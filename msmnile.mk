5e7245b7bdb5fdbd528fd429b4117a06f4ef3c0f
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-03 21:05:45 +0900

    cepheus: Import stock additional native libs list
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -316,6 +316,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Native Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
