767c511ddea22f2e8896aed0a08bd222c2012ebe
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-17 08:05:54 +0900

    Revert "cepheus: Inherit dalvik for 6gb device"
    This reverts commit a6f3d830c06fb93ea8b103511d2c0369e2d0fc08.

--- msmnile.mk
+++ msmnile.mk
@@ -23,9 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 #$(call inherit-product, vendor/google/customization/config.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/cepheus/cepheus-vendor.mk)
 
