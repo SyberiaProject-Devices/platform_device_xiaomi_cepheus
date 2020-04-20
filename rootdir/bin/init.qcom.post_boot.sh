b91cefa0054d33838e93f4f00fc9b83b6f27a83a
RenderBroken <zkennedy87@gmail.com>
2020-04-20 13:39:11 +0000

    cepheus: rootdir: Add macro to configurate UX cgroups

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -90,6 +90,35 @@ function configure_memory_parameters() {
 
 }
 
+function configure_ux_task_cgroup_membership() {
+    # EXPERIMENTAL: Optimize UX task cgroup membership
+	PIDSS=`ps -AT | grep system_server | awk '{print $3}'`
+	echo $PIDSS > /dev/cpuset/foreground/cgroup.procs
+	echo $PIDSS > /dev/stune/foreground/cgroup.procs
+
+	PIDAIO=`ps -AT | grep android.io | awk '{print $3}'`
+	echo $PIDAIO > /dev/stune/foreground/tasks
+
+	PIDAA=`ps -AT | grep android.anim | awk '{print $3}'`
+	echo $PIDAA > /dev/cpuset/top-app/tasks
+
+	PIDAALF=`ps -AT | grep android.anim.lf | awk '{print $3}'`
+	echo $PIDAALF > /dev/cpuset/top-app/tasks
+
+	PIDAFG=`ps -AT | grep android.fg | awk '{print $3}'`
+	echo $PIDAFG > /dev/stune/foreground/tasks
+
+	PIDAUI=`ps -AT | grep android.ui | awk '{print $3}'`
+	echo $PIDAUI > /dev/stune/top-app/tasks
+
+	PIDAD=`ps -AT | grep android.display | awk '{print $3}'`
+	echo $PIDAD > /dev/cpuset/top-app/tasks
+	echo $PIDAD > /dev/stune/top-app/tasks
+
+	PIDRECLAIMD=`ps -AT | grep reclaimd | awk '{print $3}'`
+	echo $PIDRECLAIMD > /dev/stune/top-app/tasks
+}
+
 case "$target" in
     "msmnile")
     # Enable EAS
@@ -243,6 +272,7 @@ case "$target" in
     find /sys/devices -name read_ahead_kb | while read node; do echo 128 > $node; done
 
     configure_memory_parameters
+    configure_ux_task_cgroup_membership
 
     echo "18432,23040,27648,32256,85296,120640" > /sys/module/lowmemorykiller/parameters/minfree
     ;;
