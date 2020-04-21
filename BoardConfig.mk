b38bc29acc0be31baa80db50cb5a3c39c2b1f0d3
EcrosoftXiao <xy19990903@gmail.com>
2020-04-21 20:27:16 +0800

    cepheus: sepolicy: Cleanup
    - Remove unused property
    - Enable SELINUX_IGNORE_NEVERALLOWS for user build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,6 +181,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
