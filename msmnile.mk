9a637b486d4e102b399e9b5c3227e3e4aad793de
DarkDampSquib <andrin.geiger1998@gmail.com>
2020-04-26 22:01:04 +0000

    cepheus: dont build camera provider
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -135,8 +135,6 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
