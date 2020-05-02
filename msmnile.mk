4e7b65efb4568bd9df685a61ec9272c0750fd9ff
EcrosoftXiao <xy19990903@gmail.com>
2020-05-02 10:47:06 +0800

    Revert "cepheus: Add Pixel thermal HAL"
    This reverts commit 1449d0c9bcd4bfd3bf8e801ed4eab2a14dc53df2.

--- msmnile.mk
+++ msmnile.mk
@@ -436,10 +436,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel
-
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msmnile
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
