91c028ff4967b446936820854682ca40839835ad
Akash <akashniki@gmail.com>
2020-05-14 11:30:43 +0200

    cephues: Build wifidisplay blobs from source

--- msmnile.mk
+++ msmnile.mk
@@ -489,9 +489,11 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl \
+    libdisplayconfig \
     libdisplayconfig.vendor \
-    libqdMetaData
+    libnl \
+    libqdMetaData \
+    libqdMetaData.system
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
