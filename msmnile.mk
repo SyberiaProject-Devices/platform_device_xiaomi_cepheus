8c83bda68993f85e6c4caaa5883045cc8a6e2bcc
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 18:19:05 +0000

    raphael: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb
    
    # Conflicts:
    #	msmnile.mk

--- msmnile.mk
+++ msmnile.mk
@@ -289,8 +289,12 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor
 
-# HotwordEnrollement
-#PRODUCT_COPY_FILES += \
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
+# Hotword Enrollment
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
