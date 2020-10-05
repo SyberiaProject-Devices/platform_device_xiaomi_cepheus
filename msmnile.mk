3ac9b40cea61a88d2d4048a8a61d0de9a2be298d
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-10-05 10:39:55 +0200

    remove APEX

--- msmnile.mk
+++ msmnile.mk
@@ -20,7 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/cepheus/cepheus-vendor.mk)
