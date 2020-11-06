5aebeb485a5dbb755830320dfcd63293cf94d1df
EcrosoftXiao <xy19990903@gmail.com>
2020-11-06 16:03:34 +0800

    cepheus: Update build fingerprint to coral Nov.
    # Conflicts:
    #	init/init_cepheus.cpp

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -81,7 +81,8 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "cepheus-user 10 QKQ1.190825.002 V12.0.2.0.QFAEUXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
+    property_override("ro.boot.verifiedbootstate", "green");
     property_override("ro.apex.updatable", "true");
 
     load_dalvikvm_properties();
