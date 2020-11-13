acc525c065cc1f0d9a2fdd11abdf1603fdfd0820
Akash Srivastava <akashniki@gmail.com>
2020-11-14 00:07:40 +0100

    cepheus: Update for R display stack
    # Conflicts:
    #	BoardConfig.mk
    #	manifest.xml
    #	msmnile.mk

--- msmnile.mk
+++ msmnile.mk
@@ -209,17 +209,20 @@ PRODUCT_PACKAGES += \
        librmnetctl
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.composer@2.4-impl \
+    android.hardware.graphics.composer@2.4-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@2.0-impl-qti-display
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display
 
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
