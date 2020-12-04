09b7617b0349fdf09fefe594801a1e3d36bd656b
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2020-12-04 15:45:24 +0100

    fix ant

--- msmnile.mk
+++ msmnile.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    com.dsi.ant.antradio_library \
+    com.dsi.and@1.0
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
