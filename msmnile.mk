d147e33306ef90a0c29b9a8bb07d202b899baef2
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2021-02-16 16:52:56 +0100

    cepheus: build oss cam stack
    # Conflicts:
    #	msmnile.mk

--- msmnile.mk
+++ msmnile.mk
@@ -127,11 +127,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libxml2 \
     libvraudio \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    libdng_sdk.vendor
-
-PRODUCT_PACKAGES += \
-    libshim_megvii
+    libdng_sdk.vendor \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
