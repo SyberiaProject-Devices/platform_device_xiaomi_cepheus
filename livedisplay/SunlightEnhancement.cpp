9ca86ae34700e6bc6252c221c686d3961bf2c478
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2021-03-01 21:05:26 +0100

    Revert "cepheus: livedisplay: Adapt dimlayer hbm sysfs path"
    This reverts commit fdf476a0df5766b5e85c4b0b15b4b82bc989fa4a.

--- livedisplay/SunlightEnhancement.cpp
+++ livedisplay/SunlightEnhancement.cpp
@@ -29,7 +29,7 @@ namespace V2_0 {
 namespace implementation {
 
 static constexpr const char* kHbmStatusPath =
-        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_hbm_en";
+        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
 
 Return<bool> SunlightEnhancement::isEnabled() {
     std::string buf;
