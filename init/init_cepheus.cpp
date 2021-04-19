4a26aa94157b85fb45812d8575f8d40c9fa447c7
mesziman <meszaros.gabor.10k@gmail.com>
2021-04-19 09:07:52 +0200

    cepheus: April fingerprint

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -100,7 +100,7 @@ static void workaround_snet_properties()
 void vendor_load_properties() {
     // fingerprint
     property_override("ro.build.description", "cepheus-user 11 RKQ1.200826.002 21.2.3 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys");
     // Workaround SafetyNet
     workaround_snet_properties();
 }
