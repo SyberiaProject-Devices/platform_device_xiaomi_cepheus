854227fe69c3a15a6a5d22715a69447b0ddb1f0b
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2021-05-07 09:16:05 +0200

    cepheus: Enable freeform window management

--- msmnile.mk
+++ msmnile.mk
@@ -219,6 +219,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
+
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
